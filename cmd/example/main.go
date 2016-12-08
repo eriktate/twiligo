f429020b50eee002c872986db5cae17b4ffd99db
Erik <etate@redventures.com>
2016-12-08 17:13:24 -0500

    Message interaction added.

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -58,6 +58,47 @@ func main() {
 
 	log.Printf("Channel from Twilio:/n%s", string(data))
 
+	//BEGINNING MESSAGE EXAMPLE
+	channelSID := channels[0].SID
+
+	// Send Message
+	log.Println("SEND MESSAGE")
+	message := twiligo.NewMessage("This is a test", "", "system")
+	sentMessage, err := client.SendMessage(channelSID, message)
+	if err != nil {
+		log.Printf("Failed to send message: %s", err)
+	}
+	message.Body = "This is also a test"
+	_, err = client.SendMessage(channelSID, message)
+
+	// Get Message
+	getMessage, err := client.Message(channelSID, sentMessage.SID)
+	if err != nil {
+		log.Printf("Failed to get message: %s", err)
+	}
+
+	// Update Message
+	getMessage.Body = "Actually this wasn't a test"
+	_, err = client.UpdateMessage(channelSID, getMessage)
+	if err != nil {
+		log.Printf("Failed to update message: %s", err)
+	}
+
+	// Get Messages
+	messages, err := client.Messages(channelSID)
+	if err != nil {
+		log.Printf("Failed to get messages: %s", err)
+	}
+
+	// Deleting Messages
+	for _, m := range messages {
+		err = client.DeleteMessage(channelSID, m.SID)
+
+		if err != nil {
+			log.Printf("Failed to delete message %s: %s", m.SID, err)
+		}
+	}
+
 	// Deleting channels
 	log.Println("DELETING CHANNELS")
 	for _, ch := range channels {
@@ -66,17 +107,5 @@ func main() {
 		if err != nil {
 			log.Printf("Failed to delete channel %s: %s", ch.SID, err)
 		}
-	}
-
-	// Verify deletion
-	log.Println("VERIFY")
-	channels, err = client.Channels()
-
-	if err != nil {
-		log.Printf("Failed to get channels: %s", err)
-	}
-
-	if len(channels) == 0 {
-		log.Println("Success!")
 	}
 }
